86787b1b24f4e3e4c8cc52e0b432cd2a25263c9e
Illia Kravchenko <il.kravchenko115@gmail.com>
2023-04-14 10:35:01 +0300

    Start learn JS and create my first script for html page

---
+++ Javascript_level_one/Part3_script.js
@@ -0,0 +1,4 @@
+var input_pounds = parseInt(prompt("What is the weight in pounds (lbs)?"));
+var kilograms = input_pounds * 0.454;
+alert("That is: " + kilograms + " kilograms");
+console.log("Conversion Completed");
