9384bb803235a07f75e95ffa8444585e3209d116
Illia Kravchenko <il.kravchenko115@gmail.com>
2023-04-15 15:25:53 +0300

    Add files where learn about for loops in JS

---
+++ Javascript_level_one/Part7_for_loop.js
@@ -0,0 +1,11 @@
+// basic for loop
+// for (var i = 0; i < 5; i++) {
+//     console.log("Number is " + i);
+// }
+
+
+// for loop via string
+var word = "ABCDEFGHIJK"
+for (let i = 0; i < word.length; i++) {
+    console.log(word[i]);
+}
