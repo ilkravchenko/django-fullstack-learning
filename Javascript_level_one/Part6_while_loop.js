99e7c11bf595d64867385c7752964fc2884898c8
Illia Kravchenko <il.kravchenko115@gmail.com>
2023-04-15 15:14:13 +0300

    Add file where learn about while loop in JS

---
+++ Javascript_level_one/Part6_while_loop.js
@@ -0,0 +1,24 @@
+var x = 0;
+
+// while (x < 5) {
+//     console.log("x is currently: " + x);
+
+//     if (x === 3) {
+//         console.log("x is equal to THREE!");
+//         break;
+//     }
+
+//     console.log("x is still less than 5, adding 1 to x");
+
+//     x = x + 1;
+// }
+
+
+// Write a while loop that prints out only even numbers from 1 to 10.
+while (x <= 10) {
+    if (x % 2 === 0) {
+        console.log("x is currently: " + x);
+    } 
+    
+    x = x + 1;
+}
