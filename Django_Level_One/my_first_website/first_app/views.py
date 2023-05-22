6042d1ac238832bd7eac62f73b803d608d022773
Illia Kravchenko <il.kravchenko115@gmail.com>
2023-05-22 16:12:39 +0300

    Create first two Templates to Django project

--- Django_Level_One/my_first_website/first_app/views.py
+++ Django_Level_One/my_first_website/first_app/views.py
@@ -3,4 +3,12 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("<h1>My First App</h1>")
+    my_dict = {'insert_me':"Hello, I'm from views.py"}
+    return render(request, 'first_app/index.html', context=my_dict)
+
+def help(request):
+    my_dict = {
+        'first_list_elem' : 10,
+        'second_list_elem' : 20,
+    }
+    return render(request, 'first_app/help.html', context=my_dict)
